ff20146e92a6fcadf696a3e0e1d2be339c504722
saiparakash <saiparakash@colanonline.com>
2022-09-27 19:37:54 +0530

    loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Header from "./Components/Header";
 import Tablecomponent from "./Components/Tablecomponent";
 import { UserProvider } from "./Context/MyContext";
+ import InitlaLoader from "./Components/InitlaLoader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 18000); ///18 seconds//
+  }, []);
   const client = new ApolloClient({
     uri: "http://localhost:5000/graphql",
     cache: new InMemoryCache(),
@@ -13,13 +20,16 @@ function App() {
 
   return (
     <div className="App">
-      <ApolloProvider client={client}>
-        <UserProvider>
-          <Header />
-          <Tablecomponent />
-        </UserProvider>
-      </ApolloProvider>
-      ,
+      {loading ? (
+        <InitlaLoader/>
+      ) : (
+        <ApolloProvider client={client}>
+          <UserProvider>
+            <Header />
+            <Tablecomponent />
+          </UserProvider>
+        </ApolloProvider>
+      )}
     </div>
   );
 }
